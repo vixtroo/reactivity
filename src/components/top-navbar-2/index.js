28494b882a37afc6e58cf21ee9d3dfd3d504d22d
vixtroo <vincentxpatrick@gmail.com>
2023-05-02 03:13:39 +0800

    made small changes in top-navbar-2

--- src/components/top-navbar-2/index.js
+++ src/components/top-navbar-2/index.js
@@ -34,7 +34,7 @@ const TopNavbar2 = () =>{
                 <div className="pt-5">
                     <Formik>
                         <Form>
-                            <div className='flex hidden h-6 rounded-sm lg:flex'>
+                            <div className='flex hidden h-6 rounded-sm md:flex'>
                                 <Field placeholder="Type keyword here..." className="search"/>
                                 <button disabled className='flex flex-wrap content-center justify-center text-white bg-red-800 rounded-r-sm h-7 w-7'>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
@@ -46,7 +46,7 @@ const TopNavbar2 = () =>{
                     </Formik>
                 </div>
             </div>
-            <button disabled className='flex mt-3 text-white lg:hidden burger'>
+            <button disabled className='flex mt-3 text-white md:hidden burger'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
